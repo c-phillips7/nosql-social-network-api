9a37e443c0ff5710c693590c5cd7a947e3578766
Chris Phillips <cp.phillips15@gmail.com>
2022-03-08 20:38:09 -0500

    added user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        unique: true,
+        required: 'username is required',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'email is required',
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    freinds: [
+
+    ]
+},
+{
+    toJSON: {
+        vitruals: true,
+        getters: true
+    },
+    id: false
+}
+);
