42c8298a6c2e271d0c8891f874d774e1905daaf8
Chris Phillips <cp.phillips15@gmail.com>
2022-03-08 23:11:03 -0500

    added logic for deleteUser then debugged to test errors, fixed for all but getUserbyId

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,13 +18,17 @@ const userController = {
     //get user by id
     getUserbyId({ params }, res) {
         User.findOne({_id:params.id})
+        // Errors out if non valid _id entered
         .populate({
             path: 'thoughts',
             select: '-__v'
         })
         .select('-__v')
         .then(dbUserData => {
-            if(!dbUserData) return res.status(404).json({message: 'No user with this id'})
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user with this id'})
+                return;
+            }
             res.json(dbUserData)
         })
     },
@@ -40,7 +44,10 @@ const userController = {
     updateUser({ params, body }, res) {
         User.findByIdAndUpdate({_id:params.id}, body, { new: true })
             .then(dbUserData => {
-                if(!dbUserData) return res.status(404).json({message: 'No user with this id'})
+                if(!dbUserData) {
+                    res.status(404).json({message: 'No user with this id'})
+                    return;
+                }
                 res.json(dbUserData)
             })
             .catch(err => res.status(400).json(err))
@@ -48,8 +55,15 @@ const userController = {
     },
 
     //delete user
-    deleteUser(){
-
+    deleteUser({ params }, res){
+        User.findOneAndDelete({_id:params.id})
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({message: 'No user with this id'})
+                    return;
+                }
+                res.json('User deleted')
+            })
     },
 
     //bonus
