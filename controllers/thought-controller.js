6f816538044c1161815f2cb283930abaffe5fd3d
Chris Phillips <cp.phillips15@gmail.com>
2022-03-08 21:18:43 -0500

    added list of functions needed for controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,7 +1,20 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
+    // get all thoughts
 
+    //get one thought by _id
+
+    // create new thought, then push thought's _id to associated user
+
+    //update thought by _id
+
+    //delete thought bu _id
+
+
+    // add reaction to thought
+
+    //delete reaction from thought
 };
 
 module.exports = thoughtController
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,19 @@
 const { User } = require('../models');
 
 const userController = {
+    //get all users
+
+    //get user by id
+
+    //create user
+
+    //update user by id
+
+    //delete user
+
+    //bonus
+        //add friend
+        //delete friend
 
 }
 
