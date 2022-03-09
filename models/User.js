2ada16e78810f1591fadd53b1cd34c660f9a5282
Chris Phillips <cp.phillips15@gmail.com>
2022-03-08 20:58:40 -0500

    added virtual to user model, and createdAt added to schema

--- models/User.js
+++ models/User.js
@@ -1,39 +1,50 @@
-const { Schema, model } = require('mongoose');
+const {
+    Schema,
+    model
+} = require('mongoose');
 
-const UserSchema = new Schema ({
+const UserSchema = new Schema({
     username: {
         type: String,
         unique: true,
         required: 'username is required',
         trim: true
     },
+    
     email: {
         type: String,
         required: 'email is required',
         unique: true,
         match: [/.+@.+\..+/, 'Must use a valid email address']
     },
-    thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }
-    ],
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //TODO: getter function to format date
+    },
+
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+
     freinds: [
 
     ]
-},
-{
+}, {
     toJSON: {
         vitruals: true,
         getters: true
     },
     id: false
-}
-);
+});
 
 //virtual for friend count
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
 
-const User = model ('User', UserSchema);
+const User = model('User', UserSchema);
 
 module.exports = User;
